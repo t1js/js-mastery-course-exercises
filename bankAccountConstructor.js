8f3e1b88953166c1fa359ba1514cfbc76ccdd28a
t1js <97554484+t1js@users.noreply.github.com>
2024-05-23 20:32:35 +0200

    small fixes for withdrawal and getBalance implementation

--- bankAccountConstructor.js
+++ bankAccountConstructor.js
@@ -55,11 +55,11 @@ function BankAccountConstructor(owner, initialBalance) {
         }
     };
     this.withdraw = function(amount) {
-        if (amount < this.balance) {
+        if (amount <= this.balance) {
             this.balance -= amount;
             console.log(`Withdrawal successful. Withdrawed ${amount}. New balance: ${this.balance}`);
         } else {
-            console.error("Insufficient funds. The withdrawal amount excedes balance.")
+            console.error("Insufficient funds. The withdrawal amount exceeds balance.")
         }
     }
 }
@@ -69,6 +69,30 @@ function BankAccountConstructor(owner, initialBalance) {
 Add a getBalance method that allows checking the current balance.
 This method should simply return the current balance without allowing any
 modifications to it. */
+
+function BankAccountConstructor(owner, initialBalance) {
+    this.owner = owner;
+    this.balance = initialBalance;
+    this.deposit = function(amount) {
+        if (amount > 0) {
+            this.balance += amount;
+            console.log(`Deposit successful. Deposited ${amount}. New balance: ${this.balance}`);
+        } else { 
+            console.error("The deposit amount must be positive.")
+        }
+    };
+    this.withdraw = function(amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            console.log(`Withdrawal successful. Withdrawed ${amount}. New balance: ${this.balance}`);
+        } else {
+            console.error("Insufficient funds. The withdrawal amount exceeds balance.")
+        }
+    };
+    this.getBalance = function() {
+        return this.balance
+    }
+}
 
 
 /* Testing and Validation:

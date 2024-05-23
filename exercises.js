786d91bc464c464681e1bd060529c1a778bf1074
t1js <97554484+t1js@users.noreply.github.com>
2024-05-23 18:05:13 +0200

    custom map function

--- exercises.js
+++ exercises.js
@@ -20,4 +20,24 @@ function squareEvenNumbers(arr) {
    }
    console.log(squareEvenNumbers([1, 2, 3, 4, 5]));
 
+//Create a function that takes an array of numbers and a callback to apply to each element.
+//Implement your version of the array map function.
+
+function mapArray(array, callback) {
+    let result = [];
+        for (let i = 0; i < array.length; i++) {
+            result.push(callback(array[i]));
+        }
+    return result;
+}
+
+//test
+
+function callback(num) {
+    return num ** 2;
+}
+
+mapArray([1, 2, 3, 4, 5], callback);
+
+
    

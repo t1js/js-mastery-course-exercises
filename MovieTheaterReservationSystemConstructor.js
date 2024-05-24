4fd2d9bfb1ee17544884397a924e8c466f0ba4ee
t1js <97554484+t1js@users.noreply.github.com>
2024-05-24 13:16:51 +0200

    some notes about tests added

--- MovieTheaterReservationSystemConstructor.js
+++ MovieTheaterReservationSystemConstructor.js
@@ -106,6 +106,8 @@ let testInstance = new MovieTheaterReservationSystemConstructor("Test Cinema", "
 testInstance.addMovie("some movie", 13, 50, 100);
 testInstance.addMovie("some other movie", 18, 40, 150);
 
+testInstance.showSchedule();
+
 //valid seat reservation
 testInstance.reserveSeat("some movie", 13);
 //valid movie, invalid time, should throw an error
@@ -118,13 +120,12 @@ testInstance.reserveSeat("invalid movie", 19);
 testInstance.purchaseTicket("some movie", 13, 100);
 //valid ticket purchase, change 168
 testInstance.purchaseTicket("some movie", 13, 268);
-
 //invalid movie, valid time, should throw an error
 testInstance.purchaseTicket("invalid movie", 13, 100);
-
 //valid movie, invalid time, should throw an error
 testInstance.purchaseTicket("some movie", 17, 100);
 //valid movie, valid time, not enough money, should throw an error
 testInstance.purchaseTicket("some movie", 13, 40);
 
-testInstance.showSchedule();
+//all tests above pass, will convert them into proper tests later
+//missing tests for adjusted prices !!

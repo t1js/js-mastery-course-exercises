a97c2ee2531f71fed66905b6615a01912d574a01
t1js <97554484+t1js@users.noreply.github.com>
2024-05-24 12:12:27 +0200

    purchase ticket method

--- MovieTheaterReservationSystemConstructor.js
+++ MovieTheaterReservationSystemConstructor.js
@@ -39,14 +39,15 @@ function MovieTheaterReservationSystemConstructor(theaterName, location) {
     this.theaterName = theaterName;
     this.location = location;
     this.movieShowings = {};
-    this.addMovie = function(movieName, showTime, seatCount) {
+    this.addMovie = function(movieName, showTime, seatCount, basePrice) {
         this.movieShowings[movieName] = {
             time: showTime,
             seats: {
                 total: seatCount,
                 available: seatCount,
                 arrangement: []
-            }
+            },
+            price: basePrice
         };
     };
     this.reserveSeat = function(movieName, showTime){
@@ -55,17 +56,45 @@ function MovieTheaterReservationSystemConstructor(theaterName, location) {
         if (movieShowing && movieShowing.time === showTime) {
             if (movieShowing.seats.available > 0) {
                 movieShowing.seats.available -= 1;
-                console.log(`You've reserved a seat for ${movieName} at ${showTime}.`)
+                console.log(`You've reserved a seat for ${movieName} at ${showTime}.`);
             } else {
-                console.error("Sorry, no available seats left.")
+                console.error("Sorry, no available seats left.");
             }
         } else {
-            console.error("No such movie at provided time.")
+            console.error("No such movie at provided time.");
         }
     };
-    this.purchaseTicket = function() {
+    this.purchaseTicket = function(movieName, showTime, paidAmount) {
+        const movieShowing = this.movieShowings[movieName];
+        const originalPrice = movieShowing.price;
 
-    };
+        if (showTime >= 0 && showTime < 12) {
+            movieShowing.price *= 0.5;
+        } else if (showTime >= 12 && showTime < 17) {
+            movieShowing.price = movieShowing.price;
+        } else if (showTime >= 17 && showTime < 24) {
+            movieShowing.price *= 1.5;
+        }
+
+        if (movieShowing && movieShowing.time === showTime) {
+            if (movieShowing.seats.available > 0) {
+                if (paidAmount === movieShowing.price) {
+                    movieShowing.seats.available -= 1;
+                    console.log(`You've purchased a ticket for ${movieName} at ${showTime}.`);
+                } else if (paidAmount > movieShowing.price) {
+                    movieShowing.seats.available -= 1;
+                    console.log(`You've purchased a ticket for ${movieName} at ${showTime}. Your change is ${paidAmount - movieShowing.price}`);
+                } else {
+                    console.error("Not enough money to purchase a ticket. Sorry!");
+                }
+            } else {
+                console.error("Sorry, no available seats left.");
+            }
+    } else {
+        console.error("No such movie at provided time.");
+    }
+    movieShowing.price = originalPrice;
+};
     this.showSchedule = function() {
         for (const movieName in this.movieShowings) {
             const movie = this.movieShowings[movieName];
